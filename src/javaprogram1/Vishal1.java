53b7c9380f8a0d6517128612f7250633c6c9707d
AkashVilasrao123 <dhageakash995@gmail.com>
2023-02-19 00:57:58 +0530

    add files

---
+++ src/javaprogram1/Vishal1.java
@@ -0,0 +1,54 @@
+package javaprogram1;
+
+import java.util.ArrayList;
+
+public class Vishal1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         
+   /*     int m = 11000;
+        int n = 12000; 
+        int o = 13000; 
+        int p = 14000;
+        
+        if (m>n && m>o && m>p ) {
+        	System.out.println("m is greatest");
+        }
+        else if (n>o) {
+        	System.out.println("n is greatest");
+        }
+        else if (o>p)	{
+        	System.out.println("o is geatest");
+        }
+        else {
+        	System.out.println("p is greatear");
+        }
+        */
+		
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		al.add(50);
+		
+		for(int i=0;i<=al.size();i++)
+		{
+			System.out.println(al.get(i));
+		}
+		System.out.println(al);
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        
+       
+	}
+
+}
